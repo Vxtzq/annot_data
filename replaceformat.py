d4f8de7807484d4a700b63e351bae49895a39aea
proplayer2020 <116555319+proplayer2020@users.noreply.github.com>
2023-04-12 18:05:45 +0200

    Create replaceformat.py

---
+++ replaceformat.py
@@ -0,0 +1,6 @@
+### Add images types here (else, your label .txt file will be named for example image.png.txt) ###
+def replace(name):
+  name = name.replace("jpg","")
+  name = name.replace("png","")
+  name = name.replace("jpeg","")
+  
