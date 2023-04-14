8c37c728391f6b0db95273eb1d21c6938e39b40a
proplayer2020 <116555319+proplayer2020@users.noreply.github.com>
2023-04-14 08:36:53 +0200

    Update traintest_split.py

--- traintest_split.py
+++ traintest_split.py
@@ -10,12 +10,12 @@ for file in glob("images/*"):
     if not "txt" in file:
         alltxt.append(file)
 file = open("result/test.txt","w")     
-for i in range(int(args.nb_images.replace("images_nb=",""))):
+for i in range(int(args.nb_images.replace("nb_images=",""))):
     name = str(alltxt[counter])
     file.write(os.path.abspath(name))
     counter +=1
 file = open("result/train.txt","w")  
-for i in range(len(alltxt)-int(args.nb_images.replace("images_nb=",""))):
+for i in range(len(alltxt)-int(args.nb_images.replace("nb_images=",""))):
     name = str(alltxt[counter])
     file.write(os.path.abspath(name))
     counter +=1
